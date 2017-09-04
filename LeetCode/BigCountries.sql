4af11f04e1ef5996c94b6b0b2d7a22fadc07970c
Michael Webb <michael.webb.developer@gmail.com>
2017-09-04 12:24:39 -0500

    Update BigCountries.sql

--- LeetCode/BigCountries.sql
+++ LeetCode/BigCountries.sql
@@ -1,3 +1,3 @@
         SELECT name, population, area
-        FROM World
-        WHERE area > 3000000 OR population > 25000000
+        FROM World wo
+        WHERE wo.area > 3000000 OR wo.population > 25000000

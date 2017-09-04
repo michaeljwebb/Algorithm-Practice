8c302fa51691a9939e22bdb76dde3ba84da58fb9
Michael Webb <michael.webb.developer@gmail.com>
2017-09-04 01:00:04 -0500

    FizzBuzz

---
+++ LeetCode/FizzBuzz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class FizzBuzz
+    {
+        private static void Main(string[] args)
+        {
+            for (int i = 1; i < 16; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+
+            }
+        }
+    }
+}

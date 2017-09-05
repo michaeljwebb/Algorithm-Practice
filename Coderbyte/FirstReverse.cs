8815813be07707a76c43e3cae0b3b715c3a4169f
Michael Webb <michael.webb.developer@gmail.com>
2017-09-05 18:33:02 -0500

    Update FirstReverse.cs

--- Coderbyte/FirstReverse.cs
+++ Coderbyte/FirstReverse.cs
@@ -12,21 +12,17 @@ namespace Coderbyte
         {
             Console.WriteLine(First_Reverse(Console.ReadLine()));
         }
-
+        
         public static string First_Reverse(string str)
         {
-
             char[] cArray = str.ToCharArray();
             string result = "";
-
             for (int i = cArray.Length - 1; i >= 0; i--)
             {
                 result += cArray[i];
                 str = result;
             }
             return str;
-
         }
-
     }
 }

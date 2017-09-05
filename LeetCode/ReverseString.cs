55b6598d10c021b34c7350301c283ecaf90845d7
Michael Webb <michael.webb.developer@gmail.com>
2017-09-05 18:32:34 -0500

    Update ReverseString.cs

--- LeetCode/ReverseString.cs
+++ LeetCode/ReverseString.cs
@@ -10,18 +10,14 @@ namespace LeetCode
     {
         public string Reverse_String(string s)
         {
-
             char[] sArray = s.ToCharArray();
             string result = "";
             for (int i = sArray.Length - 1; i >= 0; i--)
             {
                 result += sArray[i];
                 s = result;
-
             }
             return s;
-
         }
-
     }
 }

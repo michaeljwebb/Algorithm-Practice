9286d63529dab40cae64f7cb43278cfaf3517f7c
Michael Webb <michael.webb.developer@gmail.com>
2017-09-06 19:11:22 -0500

    Update MaxCount.cs

--- LeetCode/MaxCount.cs
+++ LeetCode/MaxCount.cs
@@ -1,24 +1,19 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LeetCode
-{
     class MaxCount
     {
-
         public static void Main(String[] args)
         {
             int[] nums = {2, 3, 4, 4, 5, 7, 9};
             Console.WriteLine(FindMaxCount(nums));
         }
-
-
+        
         public static int FindMaxCount(int[] nums)
         {
-            
             Dictionary<int, int> _maxCount = new Dictionary<int, int>();
             int maxIndex = 0;
             int count = 0;
@@ -27,15 +22,9 @@ namespace LeetCode
             {
                 if (_maxCount.ContainsKey(nums[i]))
                 {
-                    
+
                 }
-
-
-
             }
-
-
-
 
 
 
@@ -44,4 +33,3 @@ namespace LeetCode
         }
 
     }
-}

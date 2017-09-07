ad906e7a038e3a68181c4003d7b9719ff1311aea
Michael Webb <michael.webb.developer@gmail.com>
2017-09-07 09:54:44 -0500

    Update FizzBuzz.cs

--- LeetCode/FizzBuzz.cs
+++ LeetCode/FizzBuzz.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-    class FizzBuzz
+    public class FizzBuzz
     {
         private static void Main(string[] args)
         {

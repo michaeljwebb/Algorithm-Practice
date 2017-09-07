1dcd5dedb2b803063316eea8334ff9e500d9c831
Michael Webb <michael.webb.developer@gmail.com>
2017-09-06 20:43:30 -0500

    Max Count

--- LeetCode/MaxCount.cs
+++ LeetCode/MaxCount.cs
@@ -1,8 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace LeetCode
 {
@@ -11,37 +11,39 @@ namespace LeetCode
 
         public static void Main(String[] args)
         {
-            int[] nums = {2, 3, 4, 4, 5, 7, 9};
-            Console.WriteLine(FindMaxCount(nums));
+            int[] nums = {2, 3, 4, 9, 5, 5, 8, 5, 0};
+            FindMaxCount(nums);
         }
-
 
         public static int FindMaxCount(int[] nums)
         {
-            
             Dictionary<int, int> _maxCount = new Dictionary<int, int>();
-            int maxIndex = 0;
+            int maxKeyCount = 0;
             int count = 0;
 
             for (int i = 0; i < nums.Length; i++)
             {
-                if (_maxCount.ContainsKey(nums[i]))
+                if (!_maxCount.ContainsKey(nums[i]))
                 {
-                    
+                    _maxCount.Add(nums[i], 1);
                 }
+                else
+                {
+                    int tempCount = (int)_maxCount[nums[i]];
+                    tempCount++;
+                    _maxCount.Remove(nums[i]);
+                    _maxCount.Add(nums[i], tempCount);
 
-
-
+                    if (count < tempCount)
+                    {
+                        count = tempCount;
+                        maxKeyCount = nums[i];
+                    }
+                }
             }
-
-
-
-
-
-
-
-            return maxIndex;
+            Console.WriteLine("Max Count: " + maxKeyCount);
+            Console.WriteLine("Occurences: " + count);
+            return maxKeyCount;
         }
-
     }
 }

6060d1fa8aa12280cdd0a68d80dffbdca8ac27e5
Michael Webb <michael.webb.developer@gmail.com>
2017-09-12 17:06:13 -0500

    Create SimpleAdding.cs

---
+++ Coderbyte/SimpleAdding.cs
@@ -0,0 +1,16 @@
+using System;
+
+class MainClass {
+  public static int SimpleAdding(int num) { 
+    int result = (num*(num+1))/2;
+    num = result;
+        
+    return num;
+  }
+
+  static void Main() {  
+    // keep this function call here
+    Console.WriteLine(SimpleAdding(Console.ReadLine()));
+  } 
+   
+} 

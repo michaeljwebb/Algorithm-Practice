a26e210ad3eae75ed217761d96ae4ff5222b3183
Michael Webb <michael.webb.developer@gmail.com>
2017-10-02 12:42:03 -0500

    Update CheckNums.cs

--- Coderbyte/CheckNums.cs
+++ Coderbyte/CheckNums.cs
@@ -1,3 +1,5 @@
+//Take both parameters being passed and return the string true if num2 is greater than num1, otherwise return the string false. 
+//If the parameter values are equal to each other then return the string -1.
 using System;
 
 class MainClass {

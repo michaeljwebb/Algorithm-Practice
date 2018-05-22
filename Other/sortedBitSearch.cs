302b06088fbd98248b35dd7d2bebec05cc6dfcca
Michael Webb <michael.webb.developer@gmail.com>
2018-05-22 15:14:13 -0500

    Create sortedBitSearch.cs

---
+++ Other/sortedBitSearch.cs
@@ -0,0 +1,4 @@
+//Given an array of sorted 0s and 1s, count how many 1st exist
+//Space complexity: O(1)
+//Time complexity: O(log(N)) where N is the number of digits
+//Example: [0,0,0,0,0,1,1,1] -> 3

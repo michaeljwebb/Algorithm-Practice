af37c9760ce50febc20b524a7be2c336041df5d5
Michael Webb <michael.webb.developer@gmail.com>
2019-04-21 00:32:06 -0500

    Update Palindrome.cs

--- Other/Palindrome.cs
+++ Other/Palindrome.cs
@@ -1,28 +1,35 @@
-//Check to see if given string is a palindrome. Return true or false.
+//Check to see if given string is a palindrome. Return true or false. Ignore letter casing.
 using System;
 using System.Collections.Generic;
 
 public class Palindrome
 {
-    public static void Main()
+  public static bool Palindrome(string word)
+  {
+    string result = "";
+    bool flag = false;
+    string lowerWord = word.ToLower();
+
+    for(int i = lowerWord.Length - 1; i >= 0; i--)
     {
-        string testString = "racecar";
-        CheckPalindrome(testString);
+      result += lowerWord[i];
     }
-    
-    public static void CheckPalindrome(string s){
-        
-        string result = "";
-        char[] cArray = s.ToCharArray();
-        
-        for(int i = cArray.Length - 1; i >= 0; i--){
-            result += cArray[i];
-        }
-        if(result == s){
-            Console.WriteLine(result + " true");   
-        }
-        else if(result != s){
-            Console.WriteLine(result + " false");
-        }
+
+    if(result == lowerWord)
+    {
+      flag = true;
     }
+    else
+    {
+      flag = false;
+    }
+    return flag;
+  }
+
+	public static void Main()
+	{
+    Console.WriteLine(Palindrome("interview"));
+    Console.WriteLine(Palindrome("racecar"));
+    Console.WriteLine(Palindrome("Racecar"));
+	}
 }

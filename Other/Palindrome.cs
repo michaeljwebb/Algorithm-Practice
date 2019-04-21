874fdf56eec9e9a76c8f924cc21db7e9c5c85f79
Michael Webb <michael.webb.developer@gmail.com>
2019-04-21 00:35:08 -0500

    Update Palindrome.cs

--- Other/Palindrome.cs
+++ Other/Palindrome.cs
@@ -4,32 +4,32 @@ using System.Collections.Generic;
 
 public class Palindrome
 {
-  public static bool Palindrome(string word)
-  {
-    string result = "";
-    bool flag = false;
-    string lowerWord = word.ToLower();
+	public static bool Palindrome(string word)
+  	{
+    		string result = "";
+    		bool flag = false;
+    		string lowerWord = word.ToLower();
 
-    for(int i = lowerWord.Length - 1; i >= 0; i--)
-    {
-      result += lowerWord[i];
-    }
+    		for(int i = lowerWord.Length - 1; i >= 0; i--)
+    		{
+      		result += lowerWord[i];
+		}
 
-    if(result == lowerWord)
-    {
-      flag = true;
-    }
-    else
-    {
-      flag = false;
-    }
-    return flag;
-  }
+		if(result == lowerWord)
+		{
+		flag = true;
+		}
+		else
+		{
+		flag = false;
+		}
+		return flag;
+	}
 
 	public static void Main()
 	{
-    Console.WriteLine(Palindrome("interview"));
-    Console.WriteLine(Palindrome("racecar"));
-    Console.WriteLine(Palindrome("Racecar"));
+	Console.WriteLine(Palindrome("interview"));
+	Console.WriteLine(Palindrome("racecar"));
+    	Console.WriteLine(Palindrome("Racecar"));
 	}
 }
